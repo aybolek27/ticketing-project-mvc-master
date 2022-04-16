e008a2f6f7ad081f65e9031fba9d7456fd0c3ef4
12816 <aybolek27@gmail.com>
2022-04-16 15:06:32 -0500

    crud and other services added

---
+++ ticketing-project-mvc-master/src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,12 @@
+package com.cydeo.service;
+
+
+import com.cydeo.dto.RoleDTO;
+
+
+public interface RoleService extends CrudService<RoleDTO, Long>{
+
+
+}
+
+//save role, findById role, findAll roles
